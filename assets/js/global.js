7688e9546c40586a3d36d40c061ec154e9e10607
unknown <killcoder212@gmail.com>
2017-03-12 00:13:37 +0700

    Adding more view

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
 
+    $('#masuk').click(function(e) {
+        e.preventDefault();
+        $('#masuk-sistem').modal('show');
+
+        $('.modal-open').attr('style', '');
+    });
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 0) {
+            $('.navbar')
+                .css('height', '60px')
+                .css('background-color', 'rgba(26,188,156,0.9)');
+        } else {
+            $('.navbar')
+                .css('height', '100px')
+                .css('background-color', 'rgba(26,188,156,1)');
+        }
+    });
 });
